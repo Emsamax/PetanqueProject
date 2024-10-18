d6eb748879ac012005aaee8f5bbf72bf73d733ab
emsamax <martin.neron@laposte.net>
2024-10-18 15:38:33 +0200

    add crud func for user

--- src/main/java/com/example/demo/repository/UtilisateurRepository.java
+++ src/main/java/com/example/demo/repository/UtilisateurRepository.java
@@ -1,11 +1,13 @@
 package com.example.demo.repository;
 
 import  com.example.demo.models.Utilisateur;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.io.Serializable;
 
 @Repository
-public interface UtilisateurRepository extends CrudRepository<Utilisateur, Long> {
+
+public interface UtilisateurRepository extends CrudRepository<Utilisateur, Integer> {
 }
