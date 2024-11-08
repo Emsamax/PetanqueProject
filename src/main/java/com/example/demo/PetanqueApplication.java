e94b204e24c94ec8c966633e9719a9334fd662c2
SylvainBRUNET37 <93929648+SylvainBRUNET37@users.noreply.github.com>
2024-11-08 15:09:56 +0100

    Update main pour Java 21

--- src/main/java/com/example/demo/PetanqueApplication.java
+++ src/main/java/com/example/demo/PetanqueApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan({"com.example.demo.controllers", "com.example.demo.services", "com.example.demo.mappers"})
 public class PetanqueApplication {
 
 	public static void main(String[] args) {
