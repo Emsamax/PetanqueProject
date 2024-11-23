120a323c15f9d0919a70d8d933bd0c385dff6b06
Sylvain <sylvain.brunet37@gmail.com>
2024-11-23 09:47:57 +0100

    Commentaires utilisateur repositoyry

--- src/main/java/com/example/demo/repositories/UtilisateurRepository.java
+++ src/main/java/com/example/demo/repositories/UtilisateurRepository.java
@@ -1,11 +1,23 @@
 package com.example.demo.repositories;
 
-import  com.example.demo.models.Utilisateur;
-import org.springframework.data.jpa.repository.Query;
+import com.example.demo.models.Utilisateur;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Repository interface for managing `Utilisateur` entities.
+ * Extends `CrudRepository` to provide basic CRUD operations for the `Utilisateur` entity.
+ */
 @Repository
 public interface UtilisateurRepository extends CrudRepository<Utilisateur, Integer> {
+
+   /**
+    * Finds a `Utilisateur` by its email address.
+    * This method will automatically generate the necessary SQL query to find a user based on the email field.
+    *
+    * @param mail the email address to search for
+    * @return the `Utilisateur` entity associated with the provided email address, or `null` if no user is found
+    */
    Utilisateur findByMail(String mail);
 }
